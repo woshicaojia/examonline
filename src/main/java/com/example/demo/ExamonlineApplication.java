d9bc49831777c191b4552c1f13f0102cd698b68a
weixin <616928939@qq.com>
2018-11-09 16:56:15 +0800

    aaaaaaaaaaaaaaaaaaaa

---
+++ src/main/java/com/example/demo/ExamonlineApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamonlineApplication.class, args);
+	}
+}
