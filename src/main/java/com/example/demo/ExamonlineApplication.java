26010eaf4cb31c50fd29a197be6190b934874fa4
woshicaojia <18894037898@163.com>
2019-05-14 11:21:12 +0800

    主类代码提交

--- src/main/java/com/example/demo/ExamonlineApplication.java
+++ src/main/java/com/example/demo/ExamonlineApplication.java
@@ -13,19 +13,6 @@ import java.util.Properties;
 @SpringBootApplication
 @MapperScan("com.example.demo.dao")
 public class ExamonlineApplication extends SpringBootServletInitializer {
-    //利用PageHelp不分页的原因
-	@Bean
-	public PageHelper pageHelper() {
-        PageHelper pageHelper = new PageHelper();
-        Properties p = new Properties();
-        p.setProperty("offsetAsPageNum", "true");
-        p.setProperty("rowBoundsWithCount", "true");
-        p.setProperty("reasonable", "true");
-        p.setProperty("returnPageInfo", "check");
-        p.setProperty("params", "count=countSql");
-        pageHelper.setProperties(p);
-        return pageHelper;
- }
 	public static void main(String[] args) {
 		SpringApplication.run(ExamonlineApplication.class, args);
 	}
